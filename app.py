73907045280eda24c555c8643126938a66ee5230
Allan Vestergaard <info@avz.dk>
2022-06-21 22:11:50 +0200

    First page rendered

--- app.py
+++ app.py
@@ -1,3 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#Last Modified: 2022/06/20 10:38:54
+#Last Modified: 2022/06/21 22:10:41
+
+
+from flask import Flask, stream_with_context, render_template
+from statsgenerator import stravaClient, statsGenerator, Activity
+
+
+app = Flask(__name__)
+client=stravaClient()
+
+@app.route('/')
+def home():
+    hm=statsgenerator.filter_distance(min=5000,max=6000)
+    return render_template('index.html', data=hm)
+
+
+@app.route('/halfmarathons')
+def p_halfmarathons():
+    print("HM1")
+    hm=statsgenerator.filter_distance(min=20500,max=60000)
+    return render_template('index.html', data=hm)
+    print("HM2")
+
+
+if __name__ == '__main__':
+    
+    activities=[]
+    for activity in client.runningactivities():
+        dc=Activity(activity['start_date_local'],activity['name'],activity['distance'],activity['moving_time'],activity['average_speed'],activity['sport_type'])
+        activities.append(dc)
+    statsgenerator=statsGenerator(activities)
+    app.run()
+
---
+++ statsgenerator.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#Last Modified: 2022/06/21 22:11:20
+from dataclasses import dataclass
+from strava import Strava
+from datetime import datetime
+
+@dataclass
+class Activity:
+    start_date_local : str
+    name : str
+    distance : float
+    moving_time: float
+    average_speed: float
+    sport_type : str
+    date : datetime = None
+
+    @property
+    def tempo(self):
+        return 1/self.average_speed*1000/60
+
+
+    def __repr__(self):
+        return f"Activity({self.date}  {self.distance/1000:.2f} km   {self.name}  {self.moving_time/60:.2f} min. Tempo:{self.tempo:.2f})\n"
+
+    
+
+
+class stravaClient(Strava):
+
+    def runningactivities(self):
+        self.getToken()
+        activities=[]  
+        for page in range(1,3):
+            rv=self.getActivities(100,page)
+            if rv != []: activities=activities+rv
+            else: break     
+        print(f"ANTAL {len(activities)} hentet")       
+        for a in activities:
+            if a['sport_type']!='Run':
+                activities.remove(a)
+        print(f"ANTAL {len(activities)} efter filtrering")     
+        return activities
+
+class statsGenerator():
+    def __init__(self,rawdata): 
+        self.rawdata = rawdata
+        for activity in self.rawdata: #
+            activity.date=datetime.strptime(activity.start_date_local[0:10], "%Y-%m-%d").date()
+        self.activities = rawdata       # de data der arbejdet på efter filtrering
+        # start_date,start_date_local,distance,moving_time,average_speed,max_speed,average_cadance,
+        self._group_by_date()
+        
+    def _group_by_date(self):
+        pass
+    
+    def sort_distance(self):
+        newlist = sorted(self.activities, key=lambda d: -d.distance) 
+        return newlist
+
+    def filter_distance(self,min=0,max=100000): # i meter
+        self.activities=[activity for activity in self.activities if activity.distance >= min and activity.distance <= max]
+        return self.activities
+
+    def sort_tempo(self):
+        newlist = sorted(self.activities, key=lambda d: d.tempo) 
+        return newlist
+
+        
+if __name__ == '__main__':
+    client=stravaClient()
+    activities=[]
+    for activity in client.runningactivities():
+        dc=Activity(activity['start_date_local'],activity['name'],activity['distance'],activity['moving_time'],activity['average_speed'],activity['sport_type'])
+        activities.append(dc)
+    statsgenerator=statsGenerator(activities)
+    hm=statsgenerator.filter_distance(min=5000,max=6000)
+    print(hm)
+#    longest=statsgenerator.sort_distance()
+#    print(longest[0])
+    fastest=statsgenerator.sort_tempo()
+    print(fastest[:3])
