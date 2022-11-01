ab7b693c7fc5999b6139ad70f55c298c31d5711a
Allan Vestergaard <av@avz.dk>
2022-11-01 20:10:04 +0100

    Major change

--- stravastats/app.py
+++ stravastats/app.py
@@ -5,30 +5,32 @@
 
 from flask import Flask, stream_with_context, render_template
 from statsgenerator import stravaClient, statsGenerator, Activity
-
+from datetime import date
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 client=stravaClient()
 
 @app.route('/')
 def home():
-    return render_template('index.html', data=statsgenerator.activities_work)
+    sg.reset()
+    return render_template('index.html', data=sg.activities_work, stats=sg.basicstats())
 
 
 @app.route('/halfmarathons')
-def p_halfmarathons():
-    print("HM1")
-    
-    statsgenerator.filter(lambda a: a.distance > 21000)
-    return render_template('index.html', data=statsgenerator.activities_work)
-    print("HM2")
+def p_halfmarathons(): 
+    sg.reset()
+    sg.filter(lambda a: a.distance > 21000)
+    sg.sort(lambda a: -a.tempo)
+    print(sg.activities_work[1])
+    return render_template('index.html', data=sg.activities_work)
 
 
 if __name__ == '__main__':
     
     activities=[]
-    for activity in client.runningactivities():
+    for activity in client.runningactivities(after_date=date(2022,1,1)):
         dc=Activity(activity['start_date_local'],activity['name'],activity['distance'],activity['moving_time'],activity['average_speed'],activity['sport_type'])
         activities.append(dc)
-    statsgenerator=statsGenerator(activities)
+    sg=statsGenerator(activities)
     app.run()
