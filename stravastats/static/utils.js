f8aab27ab6dffc59bf282229405ddeefe7980e03
Allan Vestergaard <av@avz.dk>
2022-11-11 12:53:59 +0100

    Chart funktion.

--- stravastats/static/utils.js
+++ stravastats/static/utils.js
@@ -5,3 +5,13 @@ function filter() {
     var endDate = document.getElementById("endDate").value;
     window.location.href = window.location.pathname + `?distanceMin=${distanceMin}&distanceMax=${distanceMax}&startDate=${startDate}&endDate=${endDate}`;
 }
+
+function chart() {
+    var distanceMin = document.getElementById("distanceFilterMin").value;
+    var distanceMax = document.getElementById("distanceFilterMax").value;
+    var startDate = document.getElementById("startDate").value;
+    var endDate = document.getElementById("endDate").value;
+    console.log(window.location.pathname);
+    console.log(window.location.host);
+    window.location.href = "/chart" + `?distanceMin=${distanceMin}&distanceMax=${distanceMax}&startDate=${startDate}&endDate=${endDate}`;
+}
